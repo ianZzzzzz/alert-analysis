927a1d9bc4df91662b0e38d4c9d591bf46858242
ianZzzzzz <zhengyh186@gmail.com>
2020-11-19 11:12:29 +0800

    columns name

--- model/time_series.py
+++ model/time_series.py
@@ -29,7 +29,33 @@ id_list = find_useful_id(raw_data,key = 'id',value = 10)
 
 def find_useful_log(raw_data,key,value_list):
 
-    raw_data.columns = list([''])
+    
+    raw_data.columns = list([
+         "id","time","level_1","level_2",
+         "5","6",'high_temperature','temperature_alam',
+         '9','10','11','12',
+         '13','14','door_alarm','lightning_alarm',
+         '17','fan_failure','network_device_error','v1_net_error',
+         '21','22','v2_net_error','v3_net_error',
+         'v4_net_error','26','27','28',
+         'monintoring_net_connect_err','power_supply_err','31','32',
+         '33','34','35','36',
+         '37','38','voltage','current',
+         'power','42','temperature',"longtitude","magtitude" ] 
+    )
+    raw_data = raw_data[[ 
+        "id","time", 
+
+        "level_1","level_2","longtitude","magtitude", # location
+        'door_alarm', 'fan_failure','network_device_error', # device
+        'high_temperature','temperature_alam','temperature', # temperature
+        'lightning_alarm','power_supply_err','voltage','current', # power/electric
+
+        'v1_net_error','v2_net_error','v3_net_error','v4_net_error',  # network
+        'monintoring_net_connect_err' # network 
+        ]]
+
+
     raw_gb_key = raw_data.groupby(key)
     raw_gb_key = dict(list(raw_gb_key))
     for i in value_list :
